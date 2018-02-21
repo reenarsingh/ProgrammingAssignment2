16204c47499e99d85184edcc2c938c19145c3be5
reenarsingh <reenulok@gmail.com>
2018-02-21 15:11:55 -0500

    Completed R programming Assignment 2- Week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+#R Programming - Week 3 Assignment to calculate, cache and retreive the inverse of a matrix
 
-## Write a short comment describing this function
+#function to cache the matrix and the inverse of the matrix
+makeCacheMatrix <- function(origMatrix = matrix()) {
+  origgMatrix <- NULL
+  inverseMatrix <- NULL
+  
+  #set the MAtrix
+  setMatrix <- function(matrx) {
+    origgMatrix <<- matrx
+  }
+  
+  #get the matrix
+  getMatrix <- function()origMatrix
+  
+  #setInverseMatrix
+  setInverseMatrix <- function(invMatrx) {
+    inverseMatrix <<- invMatrx
+  }
+  
+  setMatrix(origMatrix)
+  #get the inverse MAtrix
+  getInverseMatrix <- function()inverseMatrix
+  
+  #return a list of all the functions
+  
+  list(setMatrix = setMatrix,  getMatrix =  getMatrix,
+       setInverseMatrix = setInverseMatrix,
+       getInverseMatrix = getInverseMatrix)
+}
 
-makeCacheMatrix <- function(x = matrix()) {
-
+#function to retrieve the invers of the matrix dromthe cache if it exists, calculate it otherwise
+cacheSolve <- function(functionObject,...){
+  inverseMatrix <- functionObject$getInverseMatrix()
+  origMatrix <- functionObject$getMatrix()
+  if(!is.null(inverseMatrix)){
+    print("Retreiving Cached Inverse for Matrix")
+    return(inverseMatrix)
+  }
+  else
+  {
+    print("calculating new Cached Inverse for Matrix")
+    functionObject$setInverseMatrix(solve(origMatrix,...))
+    return(functionObject$getInverseMatrix())
+  }
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
